033d2658be7b90f25ff64a1afa65a97e6b77ae6b
Opulent0 <96027270+Opulent0@users.noreply.github.com>
2026-06-01 19:59:35 -0500

    Initial commit [Frontend-0.0.1]

---
+++ Frontend/src/components/Cards/InfoCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="Card" class="
+    m-3
+    border-2 rounded-lg border-(--color-brand-subtle)
+    ">
+        <!-- The Header text will go here -->
+        <div class="px-4 bg-(--color-brand-dim) rounded-t-lg text-xl">
+            <slot name="Head-Text"></slot>
+        </div>
+        
+        <!-- The Body text will go here -->
+        <div class="px-4 pt-2 pb-6 text-sm">
+            <slot name="Body-Text"></slot>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+
+</script>
