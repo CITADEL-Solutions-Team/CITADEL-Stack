04dd0519b016c87a017840e572b2ae886434d64b
Opulent0 <96027270+Opulent0@users.noreply.github.com>
2026-06-27 17:38:50 -0500

    Started work on the homepage of our website.

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,7 +1,34 @@
 <template>
-
+    <Banner>
+        <template v-slot:Logo>
+            <img class="aspect-square p-5" src="./assets/CITADEL Logo.svg" alt="">
+        </template>
+    </Banner>
+    <Nav :NavItems="NavItems"/>
+    <RouterView />
 </template>
 
 <script setup lang="ts">
+    import Banner from './components/Misc/Banner.vue';
+    import Nav from './components/Misc/Nav.vue';
 
+    const NavItems: { label: string, to: string }[] = [
+        {
+            label: "About Us",
+            to: "/about-us"
+        },
+        {
+            label: "Our Mission",
+            to: "/our-mission"
+        },
+        {
+            label: "Our Services",
+            to: "/services-overview"
+        },
+        {
+            label: "Request Services",
+            to: "/contact-us"
+        },
+        
+    ]
 </script>
---
+++ Frontend/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <Hero
+    :ButtonOne="ButtonOne"
+    :ButtonTwo="ButtonTwo">
+    
+    </Hero>
+</template>
+
+<script setup lang="ts">
+    import Hero from '@/components/Sections/Hero.vue';
+
+    const ButtonOne: { label: string, to: string } = {
+        label: "About Us",
+        to: "/about-us"
+    }
+
+    const ButtonTwo: { label: string, to: string } = {
+        label: "Our Mission",
+        to: "/our-mission"
+    }
+</script>
