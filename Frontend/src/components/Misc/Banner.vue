038f529a2218f72519b91a3180d86fdc2c903c1d
Opulent0 <galacticnoob772@gmail.com>
2026-07-08 11:03:47 -0500

    Finished the About page. Deck of cards working, only need to add official roles and minor content adjustments.

--- Frontend/src/components/Misc/Banner.vue
+++ Frontend/src/components/Misc/Banner.vue
@@ -1,30 +1,32 @@
 <template>
-    <header class="w-screen h-65
+    <header class="
+    w-screen px-16
+    h-80
     bg-linear-to-r from-(--MenuCard) from-60% to-(#03191C)
     border-2 border-(--Accent)
     ">
-        <div class="px-16 py-6 flex items-center w-full h-full">
+        <div class="px-16 py-6 flex items-center h-full">
             <div class="flex-1">
                 <h1 class="
                 font-['Exo_2']
-                {{ /* Scale the banner text size with width so the logo stays visible */ }}
-                text-[64px] md:text-[80px] lg:text-[96px]
+                text-[clamp(64px,7vw,96px)]
                 ">
                     <slot name="BusinessName">
                         CITADEL SOLUTIONS
                     </slot>
                 </h1>
             </div>
-            <RouterLink 
-            :to="'\/'"
-            class="
-            h-53
-            aspect-square border-2 border-(--Accent) rounded-full 
-            flex-none">
-                <slot name="Logo">
-                    LOGO HERE
-                </slot>
-            </RouterLink>
+            <div class="h-full my-24
+                aspect-square border-2 border-(--Accent) rounded-full 
+                flex-none">
+                <RouterLink 
+                :to="'\/'"
+                class="">
+                    <slot name="Logo">
+                        LOGO HERE
+                    </slot>
+                </RouterLink>
+            </div>
         </div>
     </header>
 </template>
