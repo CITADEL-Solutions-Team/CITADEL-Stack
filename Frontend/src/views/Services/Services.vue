39e05f20eb92b4339892e48299bfb6ece4cf3641
Opulent0 <96027270+Opulent0@users.noreply.github.com>
2026-07-09 08:14:38 -0500

    Started work on the new services page

---
+++ Frontend/src/views/Services/Services.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="px-18 py-10">
+        <div class="border border-(--Accent)">
+            
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    const services = [
+        {
+            Name: "SMB Cyber Security",
+            To: "/services/cybersec"
+        },
+        {
+            Name: "Home Security",
+            To: "/services/home-sec"
+        },
+        {
+            Name: "PC Services",
+            To: "/pc-repairs"
+        },
+        {
+            Name: "Web Dev",
+            To: ""
+        }
+    ]
+</script>
