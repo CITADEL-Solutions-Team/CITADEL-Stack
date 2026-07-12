d527a1579c10f06146eff7eb58f8b2d48263e2a6
Opulent0 <96027270+Opulent0@users.noreply.github.com>
2026-07-12 17:07:39 -0500

    Created a new template for service pages making them easier to update. Uses `v-for` to render the list of services and categories removing the need for as much redundant code. Will implement into the other pages when an update to content is needed.

---
+++ Frontend/src/views/Services/updatedServicePageTemplate.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="px-16 py-8">
+
+    </div>
+</template>
+
+<script setup lang="ts">
+    import ServiceCard from '@/components/Cards/ServiceCard.vue';
+    import ServiceCategory from '@/components/Sections/ServiceCategory.vue';
+    import type { Service, Category } from '@/views/Services/Service'
+
+    const categories: Category[] = [
+        {
+            Name: "",
+            Services: []
+        }
+    ]
+</script>
