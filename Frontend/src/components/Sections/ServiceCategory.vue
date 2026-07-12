1cd5564d99f85736d033058d721dcf7793af14ac
Opulent0 <96027270+Opulent0@users.noreply.github.com>
2026-07-11 23:25:40 -0500

    Used for separating services on the service pages

---
+++ Frontend/src/components/Sections/ServiceCategory.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <div class="text-4xl pb-2 pt-4 mb-4 font-bold border-b-4 border-(--Accent)">
+            <slot name="Name"> Section Name Here </slot>
+        </div>
+        <div class="flex flex-col gap-4">
+            <slot name="Items"> Items will go here </slot>
+        </div>
+    </div>
+</template>
