2f5772ae76a25010a5609fd1207cded571ff7d18
Opulent0 <96027270+Opulent0@users.noreply.github.com>
2026-07-12 17:18:49 -0500

    Small update to the new template

--- Frontend/src/views/Services/updatedServicePageTemplate.vue
+++ Frontend/src/views/Services/updatedServicePageTemplate.vue
@@ -1,6 +1,26 @@
 <template>
     <div class="px-16 py-8">
-
+        <ServiceCategory v-for="category in categories">
+            <template v-slot:Name>
+                {{ category.Name }}
+            </template>
+            <template v-slot:Items>
+                <ServiceCard v-for="svc in category.Services">
+                    <template v-slot:>
+                        {{ svc.Name }}
+                    </template>
+                    <template v-slot:Price>
+                        {{ svc.Price }}
+                    </template>
+                    <template v-slot:List class="grid grid-cols-1 gap-1">
+                        <li v-for="item in svc.Details"> {{ item }} </li>
+                    </template>
+                    <template v-slot:Why>
+                        {{ svc.Why }}
+                    </template>                                                         
+                </ServiceCard>
+            </template>
+        </ServiceCategory>
     </div>
 </template>
 
@@ -12,7 +32,17 @@
     const categories: Category[] = [
         {
             Name: "",
-            Services: []
+            Services: [
+                {
+                    Name: "",
+                    Price: "",
+                    Details: [
+
+                    ],
+                    Why: ""
+                },
+
+            ]
         }
     ]
 </script>
