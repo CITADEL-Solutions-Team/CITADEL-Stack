f776dc7fdf82e2acfbb7c34fa57ed02321c742e3
Opulent0 <96027270+Opulent0@users.noreply.github.com>
2026-07-11 22:49:42 -0500

    Set up the template for each service page

--- Frontend/src/views/Services/CyberSec.vue
+++ Frontend/src/views/Services/CyberSec.vue
@@ -1,7 +1,22 @@
 <template>
-    <ServiceCard>
-        
-    </ServiceCard>
+    <div class="px-16 py-8">
+        <ServiceCard>
+            <template v-slot:Name>
+                
+            </template>
+            <template v-slot:Price>
+
+            </template>
+            <template v-slot:List>
+                <li>
+                    
+                </li>
+            </template>
+            <template v-slot:Why>
+
+            </template>
+        </ServiceCard>
+    </div>
 </template>
 
 <script setup lang="ts">
--- Frontend/src/views/Services/PCRepairs.vue
+++ Frontend/src/views/Services/PCRepairs.vue
@@ -1,7 +1,26 @@
 <template>
-    
+    <div class="px-16 py-8">
+        <ServiceCard>
+            <template v-slot:Name>
+                
+            </template>
+            <template v-slot:Price>
+
+            </template>
+            <template v-slot:List>
+                <li>
+                    
+                </li>
+            </template>
+            <template v-slot:Why>
+
+            </template>
+        </ServiceCard>
+    </div>
 </template>
 
 <script setup lang="ts">
+import ServiceCard from '@/components/Cards/ServiceCard.vue';
+
 
 </script>
