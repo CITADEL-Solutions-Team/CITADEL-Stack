fdf182dabd23095cc1073d6b4c2ab35c0ae3b8e3
Opulent0 <galacticnoob772@gmail.com>
2026-07-16 15:44:22 -0500

    remove the hardcoded address and also tell axios to stop bitching in the console

--- Frontend/src/api/axios.ts
+++ Frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: 'https://citadelsolutions.tech',
   timeout: 1000,
   headers: { 'X-Custom-Header': 'foobar' },
+  validateStatus: () => true,
 });
