e5c9007fee3aacef8482253b2d45cbb345a3eeb4
Opulent0 <galacticnoob772@gmail.com>
2026-08-05 17:01:57 -0500

    Started work on updating the team about page routing

--- Frontend/src/views/About/About.vue
+++ Frontend/src/views/About/About.vue
@@ -8,6 +8,9 @@
 <script setup lang="ts">
     import Deck from "@/components/Cards/BioCards/Deck.vue"
     import type { Bio } from "@/components/Cards/BioCards/BioInfo"
+    
+    // something like router/team.ts or inside your route component
+    const modules = import.meta.glob('@/views/Team/*.vue')
 
     const TeamColors: Map<string, string> = new Map([
         ["Dev", "#1e90ff"],
@@ -21,7 +24,7 @@
             Team: "CS",
             Role: "President",
             Color: "#FF1E1E",
-            To: "/about/tgeorgi",
+            To: "/about-us/tgeorgi",
             Rare: true,
         },
         {
@@ -29,7 +32,7 @@
             Team: "DEV",
             Role: "Vice President",
             Color: "#1e90ff",
-            To: "/about/dclark",
+            To: "/about-us/dclark",
             Rare: true,
         },
         {
@@ -37,7 +40,7 @@
             Team: "DEV",
             Role: "Backend Lead",
             Color: "#1e90ff",
-            To: "/about/nrutigier",
+            To: "/about-us/nrutigier",
             Rare: false,
         },
         {
@@ -45,7 +48,7 @@
             Team: "DEV",
             Role: "Frontend Dev",
             Color: "#1e90ff",
-            To: "/about/todonnell",
+            To: "/about-us/todonnell",
             Rare: false,
         },
         {
@@ -53,7 +56,7 @@
             Team: "DEV",
             Role: "Frontend Dev",
             Color: "#1e90ff",
-            To: "/about/bpalotai",
+            To: "/about-us/bpalotai",
             Rare: false,
         },
         {
@@ -61,7 +64,7 @@
             Team: "DEV",
             Role: "Frontend Dev",
             Color: "#1e90ff",
-            To: "/about/lguevara",
+            To: "/about-us/lguevara",
             Rare: false,
         }
     ]
