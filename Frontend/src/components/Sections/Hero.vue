39eef2764abe84c54c926dbc55be60564221b3c7
Opulent0 <96027270+Opulent0@users.noreply.github.com>
2026-08-17 16:24:39 -0500

    Fix small bug in hero

--- Frontend/src/components/Sections/Hero.vue
+++ Frontend/src/components/Sections/Hero.vue
@@ -32,7 +32,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, type ComputedRef } from 'vue';
+    import { computed, type ComputedRef } from 'vue';
 
     interface HeroButton {
         label: string,
