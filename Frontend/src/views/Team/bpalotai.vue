946a7e531b43d630697082af53540fe81708b2e9
Opulent0 <96027270+Opulent0@users.noreply.github.com>
2026-08-20 12:15:03 -0500

    Update all team member's pages to have temporary, standin message.

--- Frontend/src/views/Team/bpalotai.vue
+++ Frontend/src/views/Team/bpalotai.vue
@@ -1,1 +1,1 @@
-<template></template>
+<template>We're currently working on this one! <br/> Check back later please!</template>
--- Frontend/src/views/Team/dclark.vue
+++ Frontend/src/views/Team/dclark.vue
@@ -1,3 +1,3 @@
 <template>
-    
+    We're currently working on this one! <br/> Check back later please!
 </template>
