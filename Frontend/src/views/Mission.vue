d24015cee38e588ad74a4a1d6cda5283c7e31fbe
Opulent0 <96027270+Opulent0@users.noreply.github.com>
2026-08-20 16:30:59 -0500

    Create Mission Page

--- Frontend/src/views/Mission.vue
+++ Frontend/src/views/Mission.vue
@@ -1,3 +1,19 @@
 <template>
-    
+    <section class="py-8">
+        <h1 class="text-2xl font-semibold">
+            Citadel Solutions:
+        </h1>
+        <h2 class="text-4xl font-bold text-(--Accent)">
+            Making technology secure, understandable, and trustworthy — for everyone.
+        </h2>
+        <p class="text-(--SupText) pt-2 pl-4">
+            Our mission is to make secure, reliable, and accessible technology available to everyone. 
+            Citadel Solutions helps individuals, homeowners, and small businesses protect their privacy, 
+            understand their technology, and take control of the devices, data, and digital services they rely on every day. 
+            
+            Through cybersecurity, computer services, and web development, 
+            we provide practical solutions without unnecessary complexity. 
+            Everyone deserves technology they can trust and understand.
+        </p>
+    </section>
 </template>
