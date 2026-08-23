526b9e8994e97bcde137e6d75896ee2cce7b643b
Opulent0 <96027270+Opulent0@users.noreply.github.com>
2026-08-23 13:38:08 -0500

    Fix `Explore our Service Bundles` scaling for mobile

--- Frontend/src/components/Sections/ExploreServiceBundles.vue
+++ Frontend/src/components/Sections/ExploreServiceBundles.vue
@@ -5,13 +5,13 @@
         >
             Explore Our Service Bundles:
         </component>
-        <div class="px-15 pt-6">
+        <div class="lg:px-15 pt-6">
             <ul class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 <li v-for="card in props.cards"
                 class="h-60 flex flex-col bg-(--AltCard) border border-(--Accent) rounded-2xl pt-8 pb-6 px-6"
                 >
                     <div class="flex-1 content-center">
-                        <component :is="listTagType" class="text-center text-2xl self-center"> {{ card.service }} </component>
+                        <component :is="listTagType" class="text-center text-lg md:text-2xl self-center"> {{ card.service }} </component>
                     </div>
                     <RouterLink :to="card.to" class="flex w-full justify-center self-end pt-8">
                         <button class="border-2 border-(--Accent) rounded-lg w-full p-2">
